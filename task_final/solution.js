346ec293cebfc05bf0cb400e6d668a360259ee20
Ushakovaai <a9165059733@gmail.com>
2022-07-12 19:34:45 +0300

    final task fixed

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,16 +1,17 @@
-function sendRequest(name, phone, address, goods, sum){
-    let data = {client: ' ', order: {address: ' ', sum: 0}, goods: []};
+function sendRequest(name, phone, address, goods, sum)
+{
+    let dataToStringify = {data: {client: ' ', order: {address: ' ', sum: 0}, goods: []}};
     
-    data.client = `${name} ${phone}`;
-    data.order.address = `ул. ${address.street}, д. ${address.house}, подъезд ${address.entrance}, этаж ${address.floor}, кв. ${address.flat}`;
-    data.order.sum = sum;
+    dataToStringify.data.client = `${name} ${phone}`;
+    dataToStringify.data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    dataToStringify.data.order.sum = sum;
 
     let countOfGoods = goods.length;
-        for (let i = 0; i <= countOfGoods; i++){
-        data.goods.push({title: goods[i].title, count: goods[i].count});
+        for (let i = 0; i < countOfGoods; i++){
+            dataToStringify.data.goods.push({title: goods[i].title, count: goods[i].count});
     };
   
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify(dataToStringify);
 
     return jsonData;
 }
